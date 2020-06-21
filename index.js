6b7966a80a5eb138e7cb514cfcb5770d215935ef
Mohammed Sadiq <sidsmith9@gmail.com>
2020-06-21 20:47:07 +0530

    Config dotenv and change to 5000 as default PORT

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
 
 const router = require("./routes");
+
+const result = dotenv.config({ path: "./config/.env" });
+
+if (result.error) {
+  console.log(result.error);
+  throw result.error;
+}
 
 const app = express();
 
@@ -11,6 +19,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(router);
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Listening on port : ${process.env.PORT || 3000}`)
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`Listening on port : ${process.env.PORT || 5000}`)
 );
